5e5c43081750d348b3d1698ec0a15b3367037bfa
me_ritesh <ritesh.thapa@timesglobal.com.np>
2024-10-20 09:20:12 +0545

    route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\User\UserController;
 
+
+Route::get('/', [LoginController::class, 'showUserLoginForm'])->name('userLogin.form');
+
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
