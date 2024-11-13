0e35177ca7698b4f95c89159070a425102d1ec43
Ritesh Thapa <ritesh.thapa@timesglobal.com.np>
2024-11-13 11:53:02 +0545

    payslip

--- resources/views/user/payslips.blade.php
+++ resources/views/user/payslips.blade.php
@@ -48,7 +48,7 @@
                                     <a href="{{ route('user.payslipsPdf', ['start' => $range['start'], 'end' => $range['end']]) }}" class="btn btn-sm btn-primary" target="_blank">
                                         <i class="fas fa-file-alt"></i> View Payslip
                                     </a>
-                                    @else Pending
+                                    @else Your timesheet is still pending, please contact your Company
 
                                     @endif
                                 </td>
