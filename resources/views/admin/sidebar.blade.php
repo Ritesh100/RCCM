7b69a0f16fa818e50f0ff17a5f5a9aed92db3a6c
me_ritesh <ritesh.thapa@timesglobal.com.np>
2024-12-02 22:42:14 +0545

    slips

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -138,18 +138,25 @@
                     RC
                 </a>
             </li>
-            <li class="nav-item mb-2">
+            {{-- <li class="nav-item mb-2">
                 <a href="{{ route('admin.payslips') }}" 
                    class="nav-link {{ request()->routeIs('admin.payslips') }} d-flex align-items-center">
                    <i class="fas fa-money-bill me-3"></i>
                    PaySlip
                 </a>
-            </li>
+            </li> --}}
             <li class="nav-item mb-2">
                 <a href="{{ route('admin.timesheets') }}" 
                    class="nav-link {{ request()->routeIs('admin.timesheets') ? 'active' : '' }} d-flex align-items-center">
                     <i class="fas fa-clock me-3"></i>
                     Timesheet
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="{{ route('admin.payslips') }}" 
+                   class="nav-link {{ request()->routeIs('admin.payslips') ? 'active' : '' }} d-flex align-items-center">
+                   <i class="fas fa-money-bill me-3"></i>
+                    Payslips
                 </a>
             </li>
             <li class="nav-item ">
